52591363d3b0b107150eb5a428dafc7558ce9d6f
Ashwini Bhardwaj <114825460+ashwinibhardwaj@users.noreply.github.com>
2025-10-01 18:42:07 +0530

    Delete langchain_models directory

--- langchain_models/ChatModels/1_Google_Gemini.py
+++
@@ -1,8 +0,0 @@
-from langchain_google_genai import ChatGoogleGenerativeAI
-from dotenv import load_dotenv
-
-load_dotenv()
-
-model = ChatGoogleGenerativeAI(model="gemini-2.5-flash")
-result = model.invoke("What is the capital of india.")
-print(result)
